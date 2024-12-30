ffe641b6667a449780010466e07a40d8263625b6
Victor Lanciu <wlanciu@gmail.com>
2024-12-30 14:14:34 +0200

    Added homework

---
+++ HomeWork/BankAccount.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public abstract class BankAccount
+    {
+        public string AccountNumber { get; }
+        public decimal Balance { get; protected set; }
+
+        public BankAccount(string accountNumber, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            Balance = balance;
+        }
+        public abstract bool Deposit(decimal amount);
+
+        public abstract bool Withdraw(decimal amount);
+
+
+    }
+    public class SavingsAccount : BankAccount
+    {
+        public decimal InterestRate { get; private set; }
+        public SavingsAccount(string accountNumber, decimal balance, decimal interestedRate) : base(accountNumber, balance)
+        {
+            InterestRate = interestedRate;
+        }
+        public override bool Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Suma depusa trebuie sa fie mai mare ca 0");
+                return false;
+            }
+
+            Balance += amount;
+            decimal interested = Balance * InterestRate;
+            Balance += interested;
+
+            Console.WriteLine($"Suma depusa: {amount}.Dobanda aplicata: {InterestRate*100}%. Sold curent = {Balance}");
+            return true;
+        }
+        public override bool Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                Console.WriteLine("Nu dispuneti de mijloace suficiente");
+                return false;
+            }
+
+            Balance -= amount;
+            Console.WriteLine($"Suma retrasa: {amount}.Sold curent = {Balance}");
+            return true;
+        }
+    }
+
+    public class CheckingAccount : BankAccount
+    {
+        public decimal OverdraftLimit { get; private set; }
+        public CheckingAccount(string accountNumber, decimal balance, decimal overdraftLimit) : base(accountNumber, balance)
+        {
+            OverdraftLimit = overdraftLimit;
+        }
+        public override bool Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Suma depusa trebuie sa fie mai mare ca 0");
+                return false;
+
+            }
+
+            Balance += amount;
+            Console.WriteLine($"Suma depusa: {amount}.Sold curent = {Balance}");
+            return true;
+        }
+        public override bool Withdraw(decimal amount)
+        {
+            if (amount > Balance + OverdraftLimit)
+            {
+                Console.WriteLine("Nu dispuneti de mijloace suficiente, ati depasit limita de creditare");
+                return false;
+            }
+
+            Balance -= amount;
+            Console.WriteLine($"Suma retrasa: {amount}.Sold curent = {Balance}");
+            return true;
+        }
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingsAccount account = new SavingsAccount("1", 10000, 0.07m);
+            Console.WriteLine($"Sold initial = {account.Balance}");
+            account.Deposit(1233);
+            account.Withdraw(10);
+            
+
+            CheckingAccount secondAccount = new CheckingAccount("123", 50000,500);
+            Console.WriteLine($"Sold initial = {secondAccount.Balance}");
+            secondAccount.Deposit(-50);
+            secondAccount.Withdraw(800);
+            
+        }
+    }
+}
